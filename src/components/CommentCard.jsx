d7aa83c98a06565c7b2c45c69da50645694dd625
beckybp <becky.pugh@hotmail.co.uk>
2023-03-17 17:26:44 +0000

    add edit to comment card

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -4,7 +4,7 @@ export const CommentCard = ({ comment }) => {
   return (
     <section id="comment-card">
       <li>
-        <h4>{comment.author || comment.username}</h4>
+        <h4>{comment.author}</h4>
         <p>{comment.body}</p>
         <p>{convertCreatedAt(comment.created_at)}</p>
       </li>
