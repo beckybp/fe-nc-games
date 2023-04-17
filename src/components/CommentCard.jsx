bf43cb28edab166063445479f04d09a959247378
beckybp <becky.pugh@hotmail.co.uk>
2023-04-17 18:13:24 +0100

    accessibility edits to the comment cards

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -2,12 +2,10 @@ import { convertCreatedAt } from "../utils/utils";
 
 export const CommentCard = ({ comment }) => {
   return (
-    <section id="comment-card">
-      <li>
-        <h4>{comment.author}</h4>
-        <p>{comment.body}</p>
-        <p>{convertCreatedAt(comment.created_at)}</p>
-      </li>
-    </section>
+    <li id="comment-card">
+      <h4>{comment.author}</h4>
+      <p>{comment.body}</p>
+      <p>{convertCreatedAt(comment.created_at)}</p>
+    </li>
   );
 };
